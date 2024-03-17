187985d85a8c42dff38160a3c3067ee268da6268
bluecrown5@ymobile.ne.jp <bluecrown2525@gmail.com>
2024-03-17 20:46:18 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <Novice.h>
+#include "MyMath/MyMath.h"
+#include "MyMath/MatrixMath.h"
+
+const char kWindowTitle[] = "イシカワタケシ_MT3_00_04";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatirxScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatirxScreenPrintf(0, 20 * 5, rotateYMatrix, "rotateYMatrix");
+		MatirxScreenPrintf(0, 20 * 5 * 2, rotateZMatrix, "rotateZMatrix");
+		MatirxScreenPrintf(0, 20 * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
